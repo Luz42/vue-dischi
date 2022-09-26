1677d2367e82568b148eb129cc64ffc1524c89ec
Luz42 <a.evangelista93@gmail.com>
2022-09-26 18:54:34 +0200

    Creato loadingComponent
    viene visualizzato fino al momento che il server recupera i dati

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app" class="d-flex flex-column" style="height: 100vh;">
     <headerComponent/>
-    <mainComponent :listAlbum="cards"/>
+    <loadingComponent v-if="loading"/>
+    <mainComponent v-else :listAlbum="cards"/>
   </div>
 </template>
 
 <script>
 import headerComponent from './components/headerComponent.vue';
 import mainComponent from './components/mainComponent.vue';
+import loadingComponent from './loadingComponent.vue';
 import axios from 'axios';
 
 export default {
   name: 'App',
   data(){return{
-    cards:[]
+    cards:[],
+    loading: true
   }},
   created(){
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(({data, status})=>{
+
       if(status === 200){
+        this.loading = false
         console.log(data.response)
         this.cards = data.response
         console.log(this.cards)
       }
       else{
+        this.loading = false
         data.success = false
       }
     })
@@ -31,7 +37,8 @@ export default {
   },
   components: {
     headerComponent,
-    mainComponent
+    mainComponent,
+    loadingComponent
   },
 }
 </script>
---
+++ src/loadingComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-grow-1 d-flex justify-content-center align-items-center">
+
+    <div class="spinner-border text-success"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+    @import '@/assets/variables.scss';
+    div.flex-grow-1{
+        background-color: $bg-main;
+    }
+</style>
